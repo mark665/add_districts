c28e1a1e3cdbd1396e0b2fa27d6b5fcce7f08f99
noah <noah@ubuntu.(null)>
2012-04-29 15:08:17 -0700

    Django install, ad app added.

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'add_districts.views.home', name='home'),
+    # url(r'^add_districts/', include('add_districts.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
