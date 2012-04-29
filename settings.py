f05c30623b7f90272372d8f3102cd6b247290d71
Noah Glusenkamp <noah@adddistricts.com>
2012-04-29 16:21:34 -0700

    Added admin page.

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'adddistricts',                      # Or path to database file if using sqlite3.
         'USER': 'glusenkamp',                      # Not used with sqlite3.
-        'PASSWORD': adpasswords.glusenkamp,          # See setup in 
+        'PASSWORD': adpasswords.glusenkamp ,          # See imports above
         'HOST': 'ec2-23-21-20-184.compute-1.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
