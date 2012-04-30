edc487ea98e3797fa82d8ca688ede8a2d13f0e72
Noah Glusenkamp <noah@adddistricts.com>
2012-04-29 17:09:56 -0700

    Bare beginnings of a datamodel in models.py

--- ad/models.py
+++ ad/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Version(models.Model):
+	
+  name = models.CharField(max_length=50)	
+  pub = models.CharField(max_length=100)
+  pub_url = models.CharField(max_length=200)
+  date_valid = models.DateField()
+  date_invalid = models.DateField()
+
+  source_url = models.CharField(max_length=250)
+  date_addded = models.DateField()
+
+  def __unicode__(self):
+  	return self.name
+
+class CD(models.Model):
+
+  version = models.ForeignKey(Version)
+  
+
+
+
+
+  def __unicode__(self):
+  	return str(self)
+
