c3ac97255ac9b1fb72d46a0f9cb6c9016deaab5d
Mark Price <mprice@ubuntu.(none)>
2012-05-05 16:06:30 -0700

    adding admin interface for congressional districts

--- ad/admin.py
+++ ad/admin.py
@@ -1,25 +1,5 @@
 from django.contrib import admin
-from ad.models import Congress_Districts, Version
+from django.contrib.gis import admin
+from ad.models import Congress_Districts
 
-class VersionAdmin(admin.ModelAdmin):
-  list_display = (
-		  'name',
-		  'pub',
-		  'pub_url',
-		  'date_valid',
-		  'date_invalid',
-		  'source_url',
-		  'date_addded',
-		 )
-
-class CongressAdmin(admin.ModelAdmin):
-  list_display = (
-		  'state_fips',
-		  'cd_fips',
-		  'geo_id',
-		  'name',
-		  'cd_session',
-		 )
-
-admin.site.register(Congress_Districts,CongressAdmin)
-admin.site.register(Version,VersionAdmin)
+admin.site.register(Congress_Districts, admin.OSMGeoAdmin)
