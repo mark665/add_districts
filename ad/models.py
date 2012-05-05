1c20461d65776ac6a37d6446d8eb824360df9f2e
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2012-05-05 15:49:03 -0700

    Changed model to mimic Census attributes

--- ad/models.py
+++ ad/models.py
@@ -1,31 +1,21 @@
 from django.contrib.gis.db import models
 
-class Version(models.Model):
-	
-  name = models.CharField(max_length=50)	
-  pub = models.CharField(max_length=100)
-  pub_url = models.CharField(max_length=200)
-  date_valid = models.DateField()
-  date_invalid = models.DateField()
-  source_url = models.CharField(max_length=250)
-  date_addded = models.DateField()
-
-  def __unicode__(self):
-  	return self.name
-
 class Congress_Districts(models.Model):
-
-  version = models.ForeignKey(Version)
-
-  state_fips = models.CharField(max_length=2)
-  cd_fips = models.CharField(max_length=2)
-  geo_id = models.CharField(max_length=7)
-  name = models.CharField(max_length=50)
-  cd_session = models.CharField(max_length=3)
-
-  mpoly = models.MultiPolygonField()
+  statefp = models.CharField(max_length=2)
+  cd112fp = models.CharField(max_length=2)
+  geoid = models.CharField(max_length=7)
+  namelsad = models.CharField(max_length=41)
+  lsad = models.CharField(max_length=2)
+  cdsessn = models.CharField(max_length=3)
+  mtfcc = models.CharField(max_length=5)   
+  funcstat = models.CharField(max_length=1)
+  aland = models.FloatField()
+  awater = models.FloatField()
+  intptlat = models.CharField(max_length=11)
+  intptlon = models.CharField(max_length=12)
+  geom = models.MultiPolygonField(srid=4326)
   objects = models.GeoManager()
 
   def __unicode__(self):
-  	return self.name
+    return self.namelsad
 
