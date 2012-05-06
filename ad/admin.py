35f1d57b0051482e0962084a528a86da22f42a4d
Mark Price <mprice@ubuntu.(none)>
2012-05-05 18:42:48 -0700

    adding templates folder

--- ad/admin.py
+++ ad/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.gis import admin
-from ad.models import Congress_Districts
+from ad.models import Congress_Districts, Counties, States
 
+admin.site.register(States, admin.OSMGeoAdmin)
+admin.site.register(Counties, admin.OSMGeoAdmin)
 admin.site.register(Congress_Districts, admin.OSMGeoAdmin)
