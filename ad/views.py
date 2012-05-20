ac30f2f2005f22b555be103e1dbe3257005ea6af
Mark Price <mprice@ubuntu.(none)>
2012-05-20 13:54:49 -0700

    correcting merge residuals that caused loading erors

--- ad/views.py
+++ ad/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-=======
 from django.template import Context,loader
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
