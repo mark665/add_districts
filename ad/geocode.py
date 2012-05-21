f5a9fe673baa32732f8aface93ad13085a4417e9
Noah Glusenkamp <noah@ubuntu.(none)>
2012-05-21 02:33:14 -0700

    Added to geocode.py

--- ad/geocode.py
+++ ad/geocode.py
@@ -1,17 +1,48 @@
-import tempfile
-import shutil
+# Geocodes Document class file
+import requests
+import csv, sys
+import simplejson
 
-FILE_UPLOAD_DIR = '/cache/upload'
+mapquest_osm_url = 'http://open.mapquestapi.com/nominatim/v1/search'
+mapquest_url = 'http://www.mapquestapi.com/geocoding/v1/address'
 
-def handle_uploaded_file(source):
-    #fd, filepath = tempfile.mkstemp(prefix=source.name, dir=FILE_UPLOAD_DIR)
-    # with open(filepath, 'a') as dest:
-        # shutil.copyfile(source, dest)
-    # return filepath
+
+def handle_uploaded_file(uploaded_file, districts_requested):
+
+    # Create id variable for each row processed
+    row_id = 1
+
+    results = []
+
+    addresses = uploaded_file.read().split('\n')
+
     
-    with open (source) as f:
-        read_data = f.read()
-    f.closed
-    
-    def __unicode__(self):
-        return read_data
+    for address in addresses:
+        line = []
+
+        line.append(row_id)
+        row_id += 1
+ 
+        line.append(address)
+
+        payload = {
+          'format': 'json',
+          'q': address,
+          'addressdetails': '1',
+          'limit' : '1',
+        }
+
+        r = requests.get(mapquest_osm_url, params=payload)
+        c = r.content
+        j = simplejson.loads(c)
+
+        line.append(j)
+
+        results.append(line)
+
+
+               
+
+
+
+    return results
--- ad/views.py
+++ ad/views.py
@@ -1,4 +1,3 @@
-
 from ad.models import *
 from ad.forms import *
 from ad.geocode import handle_uploaded_file
@@ -15,25 +14,39 @@ from ad.models import *
 
 @login_required
 def home(request):
-    #Handle file upload
+    # Handle file upload
     if request.method == "POST":
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile = request.FILES['docfile'])
             newdoc.save()
-            #contents = handle_uploaded_file(newdoc)
-            #Redirect to the document list after POST
-            #return HttpResponseRedirect(reverse('ad.views.list'))
-            return HttpResponseRedirect(reverse('ad.views.home'),{'contents': contents})
-    else:
+
+            contents = handle_uploaded_file(newdoc.docfile, request.POST.getlist('district'))
+
+            # Load list of documents in cache dir
+            documents = Document.objects.all()
+
+            # Render home page with the documents, form, requested districts
+            return render_to_response(
+                'ad/index.html',
+                {'documents': documents, 'form': form, 'districts_requested': request.POST.getlist('district'), 'contents': contents},
+                context_instance=RequestContext(request)
+            )
+
+        # Bounce back to home page if file not included
+        else:
+            return HttpResponseRedirect(reverse('ad.views.home'))
+           
     #Load homepage (request.method = 'GET')
-        form = DocumentForm() # A empty unbound form
+    else:
+        # A empty unbound form
+        form = DocumentForm() 
             
-    #Load list of documents in cache dir
-    documents = Document.objects.all()
+        # Load list of documents in cache dir
+        documents = Document.objects.all()
     
-    # Render home page with the documents and the form
-    return render_to_response(
+        # Render home page with the documents and the form
+        return render_to_response(
         'ad/index.html',
         {'documents': documents, 'form': form},
         context_instance=RequestContext(request)
