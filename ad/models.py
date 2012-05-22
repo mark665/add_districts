f2d2cdc1e7eb6838cc650bffaf43f101a30df894
Mark Price <mprice@ubuntu.(none)>
2012-05-22 00:37:07 -0700

    removing old document model

--- ad/models.py
+++ ad/models.py
@@ -91,12 +91,6 @@ class Blocks(models.Model):
     return self.name
 
 
-#class to handle file upload    
-class Document(models.Model):
-  #docfile = models.FileField(upload_to='documents/%Y/%m/%d')
-  docfile = models.FileField(upload_to='upload/')
-  
-
 def get_media_upload_dir(instance, filename):
     user_id  = instance.user.id
     upload_dir = "%s/%d/%s" % (settings.MEDIA_ROOT, user_id, filename)
