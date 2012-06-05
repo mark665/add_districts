1b6cecf6ab3605428279bec4bef44e405704b9f6
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2012-06-05 00:46:42 -0700

    Some style tweaks.

--- ad/forms.py
+++ ad/forms.py
@@ -9,7 +9,8 @@ CHOICES = (
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
         label = 'Select a File',
-        help_text = 'max. 42 megabytes'
+        help_text = 'max. 42 megabytes',
+        
     )
     letters = forms.MultipleChoiceField(
             choices=CHOICES, 
